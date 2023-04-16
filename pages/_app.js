7a9c210066be4ced29047768dc47440b3f1ff647
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-04-16 20:47:45 -0300

    add first images and layaut

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Layaut from '../components/Layaut'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layaut>
+      <Component {...pageProps} />
+    </Layaut>
+  )
+
 }
 
 export default MyApp
